d73123b38777ecfb20a7df3265900ed576a1b529
wlgnss0912 <jeonjihoon93@naver.com>
2022-01-30 23:32:03 +0900

    22.01.30 객체지향의 원리 적용 OCP, DIP

---
+++ src/main/java/hello/core2/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core2.discount;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {//ctrl + shift + T : 테스트 만들기
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
--- src/main/java/hello/core2/order/OrderServiceImpl.java
+++ src/main/java/hello/core2/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core2.order;
 
 import hello.core2.discount.DiscountPolicy;
-import hello.core2.discount.FixDiscountPolicy;
 import hello.core2.member.Member;
 import hello.core2.member.MemberRepository;
 import hello.core2.member.MemoryMemberRepository;
@@ -9,7 +8,7 @@ import hello.core2.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
